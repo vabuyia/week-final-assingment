ba9a716761113458cbee02b4e57751893118ec1a
Abuya Obunga <vabuyia@gmail.com>
2021-11-20 17:54:17 +0300

    Add semicolon to js file

--- form.js
+++ form.js
@@ -3,18 +3,19 @@ const email = document.getElementById('email');
 const message = document.getElementById('small');
 
 function validateEmail(email) {
-  const regEx = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/
+  const regEx = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
   if (regEx.test(email)) {
-    return true
-  };
-  return false
-};
+    return true;
+  }
+  return false;
+}
 
 form.addEventListener('submit', (e) => {
   if (!validateEmail(email.value)) {
-    message.innerText = '(*) Please provide a email in lower case'
-    e.preventDefault()
+    message.innerText = '(*) Please provide a email in lower case';
+    e.preventDefault();
   } else {
-    form.submit()
+    form.submit();
   }
-})
+});
+
